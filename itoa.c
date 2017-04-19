b8539e6f602ce3df1de1d085fb603a5e7350f3cd
Engi-xm <kristijonas.a@gmail.com>
2017-04-20 00:19:05 +0300

    initial itoa

---
+++ itoa.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* itoa(int num) {
+	int sign, temp, n, x;
+	char* str;
+
+	if((sign = num) < 0) // if negative
+		num = -num; // make positive
+	temp = num; // record to temp
+	while(temp > 0) {
+		n++; // count figures
+		temp /= 10; // cycle
+	}
+	if(sign < 0) {
+		str = (char*)malloc((n + 2) * sizeof(char)); // allocate memory
+		*str = '-'; // push sign
+		while(x < n) {
+			*(str + n - x++) = ((num % 10) + '0'); // set char
+			num /= 10; // cycle
+		}
+		*(str + n + 2) = '\0'; // close string
+	} else {
+		str = (char*)malloc((n + 1) * sizeof(char)); // allocate memory
+		while(++x <= n) {
+			*(str + n - x) = ((num % 10) + '0'); // set char
+			num /= 10; // cycle
+		}
+		*(str + n + 1) = '\0'; // close string
+	}
+
+	return str;
+}
+
+int main(void) {
+	int a = -154;
+	char* ptr_str_a = itoa(a);
+	
+	printf("int a = %s \n", ptr_str_a);
+
+	free(ptr_str_a);
+
+	return 0;
+}

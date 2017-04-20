0d5c2ace316d5792a4f8f7f81954ff28e0a366a6
Engi-xm <kristijonas.a@gmail.com>
2017-04-20 18:13:16 +0300

    init vars for reliability

--- itoa.c
+++ itoa.c
@@ -4,6 +4,7 @@
 char* itoa(int num) {
 	int sign, temp, n, x;
 	char* str;
+	x = n = 0;
 
 	if((sign = num) < 0) // if negative
 		num = -num; // make positive
@@ -22,8 +23,8 @@ char* itoa(int num) {
 		*(str + n + 2) = '\0'; // close string
 	} else {
 		str = (char*)malloc((n + 1) * sizeof(char)); // allocate memory
-		while(++x <= n) {
-			*(str + n - x) = ((num % 10) + '0'); // set char
+		while(x < n) {
+			*(str + n - ++x) = ((num % 10) + '0'); // set char
 			num /= 10; // cycle
 		}
 		*(str + n + 1) = '\0'; // close string
@@ -33,12 +34,16 @@ char* itoa(int num) {
 }
 
 int main(void) {
-	int a = -154;
+	int a = 5454;
+	int b = -154;
 	char* ptr_str_a = itoa(a);
+	char* ptr_str_b = itoa(b);
 	
 	printf("int a = %s \n", ptr_str_a);
+	printf("int b = %s \n", ptr_str_b);
 
 	free(ptr_str_a);
+	free(ptr_str_b);
 
 	return 0;
 }
